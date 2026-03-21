4d486796f16fe4b90b3e805f864064da77a5cb66
jemhakdog <jemcarlo46@gmail.com>
2026-03-21 18:03:42 +0800

    test fix 2

--- heritage_models/__init__.py
+++ heritage_models/__init__.py
@@ -1,1 +1,17 @@
-# Module initialization
+from .built_heritage import BuiltHeritage, BUILT_HERITAGE_DETAIL
+from .natural_heritage import NaturalHeritage, NATURAL_HERITAGE_DETAIL
+from .movable_heritage import MovableHeritage, MOVABLE_HERITAGE_DETAIL
+from .intangible_heritage import IntangibleHeritage, INTANGIBLE_HERITAGE_DETAIL
+from .personality_profile import PersonalityProfile, PERSONALITY_DETAIL
+from .cultural_institution import CulturalInstitution, INSTITUTION_DETAIL
+from .lgu_culture_program import LGUCultureProgram, LGU_PROGRAM_DETAIL
+
+__all__ = [
+    'BuiltHeritage', 'BUILT_HERITAGE_DETAIL',
+    'NaturalHeritage', 'NATURAL_HERITAGE_DETAIL',
+    'MovableHeritage', 'MOVABLE_HERITAGE_DETAIL',
+    'IntangibleHeritage', 'INTANGIBLE_HERITAGE_DETAIL',
+    'PersonalityProfile', 'PERSONALITY_DETAIL',
+    'CulturalInstitution', 'INSTITUTION_DETAIL',
+    'LGUCultureProgram', 'LGU_PROGRAM_DETAIL'
+]
